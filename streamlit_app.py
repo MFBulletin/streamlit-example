b0c9a5bafbcecbc800b5d4f13f36ad4be252dd41
MFBulletin <135762006+MFBulletin@users.noreply.github.com>
2023-08-17 14:08:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,9 @@ with st.echo(code_location='below'):
 
 
 # Insert a chat message container.
->>> with st.chat_message("user"):
->>>    st.write("Hello 👋")
->>>    st.line_chart(np.random.randn(30, 3))
+with st.chat_message("user"):
+   st.write("Hello 👋")
+   st.line_chart(np.random.randn(30, 3))
 
 # Display a chat input widget.
->>> st.chat_input("Say something")
+ st.chat_input("Say something")
